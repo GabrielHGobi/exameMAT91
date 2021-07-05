de7995ca6ef73dc9f7d1408e87e44d73ecf050c5
GabrielHGobi <67982371+GabrielHGobi@users.noreply.github.com>
2021-07-05 12:46:34 -0300

    Adapted all the simple step methods

--- passos_simples.jl
+++ passos_simples.jl
@@ -10,13 +10,13 @@ export euler, euler_melhorado, runge_kutta_4_ordem. runge_kutta_fehlberg
 # a     real, tempo inicial
 # b     real, tempo final
 # N     inteiro, número de passos
-# x0     real, condição inicial da primeira variavel
-# y0     real, condição inicial da segunda variavel
-# fx     função lado direito do PVI para a primeira variavel
-# fy     função lado direito do PVI para a segunda variavel
+# x0    real, condição inicial da primeira variavel
+# y0    real, condição inicial da segunda variavel
+# fx    função lado direito do PVI para a primeira variavel
+# fy    função lado direito do PVI para a segunda variavel
 # ---------------------------------------------------------------------
 # Saída:
-# U     solução aproximada de u' = f(t,u)
+# t, X, Y     solução aproximada do sistema linear
 
 function euler(fx, fy,a::Float64,b::Float64,N::Int64,x0::Float64, y0::Float64)
     k = (b-a)/N
@@ -42,24 +42,29 @@ end
 # a     real, tempo inicial
 # b     real, tempo final
 # N     inteiro, número de passos
-# u     real, condição inicial
-# f     função lado direito do PVI
+# x0     real, condição inicial da primeira variavel
+# y0     real, condição inicial da segunda variavel
+# fx     função lado direito do PVI para a primeira variavel
+# fy     função lado direito do PVI para a segunda variavel
 # ---------------------------------------------------------------------
 
 # Saída:
-# U     solução aproximada de u' = f(t,u)
+# t, X, Y     solução aproximada do sistema linear
 
-function euler_melhorado(f,a::Float64,b::Float64,N::Int64,u::Float64)
+function euler_melhorado(fx, fy,a::Float64,b::Float64,N::Int64,x0::Float64, y0::Float64)
     k = (b-a)/N
-    U = zeros(N+1)
+    X = zeros(N+1)
+    Y = zeros(N+1)
     t = zeros(N+1)
-    U[1] = u
+    X[1] = x0
+    Y[1] = y0
     t[1] = a
     for i = 1:N
-        U[i+1] = U[i] + k*f(t[i]+k/2,U[i]+k/2*f(t[i], U[i]))
+        X[i+1] = X[i] + k*fx(t[i]+k/2, X[i]+k/2*fx(t[i], X[i], Y[i]), Y[i]+k/2*fy(t[i], X[i], Y[i]))
+        Y[i+1] = Y[i] + k*fy(t[i]+k/2, X[i]+k/2*fx(t[i], X[i], Y[i]), Y[i]+k/2*fy(t[i], X[i], Y[i]))
         t[i+1] = t[i] + k
     end
-    return t,U
+    return t,X,Y
 end
 
 # =====================================================================
@@ -70,28 +75,37 @@ end
 # a     real, tempo inicial
 # b     real, tempo final
 # N     inteiro, número de passos
-# u     real, condição inicial
-# f     função lado direito do PVI
+# x0     real, condição inicial da primeira variavel
+# y0     real, condição inicial da segunda variavel
+# fx     função lado direito do PVI para a primeira variavel
+# fy     função lado direito do PVI para a segunda variavel
 # ---------------------------------------------------------------------
 
 # Saída:
-# U     solução aproximada de u' = f(t,u)
+# t, X, Y     solução aproximada do sistema linear
 
-function runge_kutta_4_ordem(f,a::Float64,b::Float64,N::Int64,u::Float64)
+function runge_kutta_4_ordem(fx, fy,a::Float64,b::Float64,N::Int64,x0::Float64, y0::Float64)
     k = (b-a)/N
-    U = zeros(N+1)
+    X = zeros(N+1)
+	Y = zeros(N+1)
     t = zeros(N+1)
-    U[1] = u
+    X[1] = x0
+	Y[1] = y0
     t[1] = a
     for i = 1:N
-        F0 = f(t[i], U[i])
-        F1 = f(t[i] + k/2, U[i] + 1/2*F0)
-        F2 = f(t[i] + k/2, U[i] + 1/2*F1)
-        F3 = f(t[i] + k, U[i] + F2)
-        U[i+1] = U[i] + k/6*(F0+2*F1+2*F2+F3)
-        t[i+1] = t[i] + k
+        F0x = fx(t[i], X[i], Y[i])
+		F0y = fy(t[i], X[i], Y[i])
+        F1x = fx(t[i] + k/2, X[i] + 1/2*F0x, Y[i] + 1/2*F0y)
+		F1y = fy(t[i] + k/2, X[i] + 1/2*F0x, Y[i] + 1/2*F0y)
+        F2x = fx(t[i] + k/2, X[i] + 1/2*F1x, Y[i] + 1/2*F1y)
+		F2y = fy(t[i] + k/2, X[i] + 1/2*F1x, Y[i] + 1/2*F1y)
+        F3x = fx(t[i] + k, X[i] + F2x, Y[i] + F2y)
+		F3y = fy(t[i] + k, X[i] + F2x, Y[i] + F2y)
+        X[i+1] = X[i] + k/6*(F0x+2*F1x+2*F2x+F3x)
+		Y[i+1] = Y[i] + k/6*(F0y+2*F1y+2*F2y+F3y)
+		t[i+1] = t[i] + k	
     end
-    return t,U
+    return t,X,Y
 end
 
 # =====================================================================
@@ -104,36 +118,54 @@ end
 # Kmax  real, tamanho do passo máximo
 # Kmin  real, tamanho do passo mínimo
 # tol   real, tolerância máxima per
-# u     real, condição inicial
-# f     função lado direito do PVI
+# x0     real, condição inicial da primeira variavel
+# y0     real, condição inicial da segunda variavel
+# fx     função lado direito do PVI para a primeira variavel
+# fy     função lado direito do PVI para a segunda variavel
 # ---------------------------------------------------------------------
 
 # Saída:
-# U     solução aproximada de u' = f(t,u)
+# t, X, Y     solução aproximada do sistema linear
 
-function runge_kutta_fehlberg(f,a::Float64,b::Float64,tol::Float64, Kmax::Float64, Kmin::Float64, u::Float64)
+function runge_kutta_fehlberg(fx, fy, a::Float64,b::Float64,tol::Float64, Kmax::Float64, Kmin::Float64, x0::Float64, y0::Float64)
     N = trunc(Int, (b-a)/Kmin)
-    U = zeros(N+1)
+    X = zeros(N+1)
+	Y = zeros(N+1)
     t = zeros(N+1)
+    X[1] = x0
+	Y[1] = y0
+    t[1] = a
     flag::Bool = true
     i = 1
     delta = 0.0
     k = Kmax
-    U[1] = u
-    t[1] = a
     while flag
-        F0 = k*f(t[i], U[i])
-        F1 = k*f(t[i] + k/4, U[i] + 1/4*F0)
-        F2 = k*f(t[i] + 3*k/8, U[i] + 3/32*F0 + 9/32*F1)
-        F3 = k*f(t[i] + 12*k/13, U[i] + 1932/2197*F0 - 7200/2197*F1 + 7296/2197*F2)
-        F4 = k*f(t[i] + k, U[i] + 439/216*F0 - 8*F1 + 3680/513*F2 - 845/4104*F3)
-        F5 = k*f(t[i] + k/2, U[i] - 8/27*F0 + 2*F1 - 3544/2565*F2 + 1859/4104*F3 - 11/40*F4)
+        F0x = k*fx(t[i], X[i], Y[i])
+        F0y = k*fy(t[i], X[i], Y[i])
+        F1x = k*fx(t[i] + k/4, X[i] + 1/4*F0x, Y[i] + 1/4*F0y)
+        F1y = k*fy(t[i] + k/4, X[i] + 1/4*F0x, Y[i] + 1/4*F0y)
+        F2x = k*fx(t[i] + 3*k/8, X[i] + 3/32*F0x + 9/32*F1x, Y[i] + 3/32*F0y + 9/32*F1y)
+        F2y = k*fy(t[i] + 3*k/8, X[i] + 3/32*F0x + 9/32*F1x, Y[i] + 3/32*F0y + 9/32*F1y)
+        F3x = k*fx(t[i] + 12*k/13, X[i] + 1932/2197*F0x - 7200/2197*F1x + 7296/2197*F2x, Y[i] + 1932/2197*F0y - 7200/2197*F1y + 7296/2197*F2y)
+        F3y = k*fy(t[i] + 12*k/13, X[i] + 1932/2197*F0x - 7200/2197*F1x + 7296/2197*F2x, Y[i] + 1932/2197*F0y - 7200/2197*F1y + 7296/2197*F2y)
+        F4x = k*fx(t[i] + k, X[i] + 439/216*F0x - 8*F1x + 3680/513*F2x - 845/4104*F3x, Y[i] + 439/216*F0y - 8*F1y + 3680/513*F2y - 845/4104*F3y)
+        F4y = k*fy(t[i] + k, X[i] + 439/216*F0x - 8*F1x + 3680/513*F2x - 845/4104*F3x, Y[i] + 439/216*F0y - 8*F1y + 3680/513*F2y - 845/4104*F3y)
+        F5x = k*fx(t[i] + k/2, X[i] - 8/27*F0x + 2*F1x - 3544/2565*F2x + 1859/4104*F3x - 11/40*F4x, Y[i] - 8/27*F0y + 2*F1y - 3544/2565*F2y + 1859/4104*F3y - 11/40*F4y)
+        F5y = k*fy(t[i] + k/2, X[i] - 8/27*F0x + 2*F1x - 3544/2565*F2x + 1859/4104*F3x - 11/40*F4x, Y[i] - 8/27*F0y + 2*F1y - 3544/2565*F2y + 1859/4104*F3y - 11/40*F4y)
 
-        R = 1/k*abs(1/360*F0 - 128/4275*F2 - 2197/75240*F3 + 1/50*F4 + 2/55*F5)
+        F0 = [F0x, F0y]
+        F1 = [F1x, F1y]
+        F2 = [F2x, F2y]
+        F3 = [F3x, F3y]
+        F4 = [F4x, F4y]
+        F5 = [F5x, F5y]
+
+        R = 1/k*norm(1/360*F0 - 128/4275*F2 - 2197/75240*F3 + 1/50*F4 + 2/55*F5)
 
         if R <= tol
             t[i+1] = t[i] + k
-            U[i+1] = U[i] + 25/216*F0 + 1408/2565*F2 + 2197/4104*F3 - 1/5*F4
+            X[i+1] = X[i] + 25/216*F0x + 1408/2565*F2x + 2197/4104*F3x - 1/5*F4x
+            Y[i+1] = Y[i] + 25/216*F0y + 1408/2565*F2y + 2197/4104*F3y - 1/5*F4y
             i = i+1
         end
 
@@ -162,8 +194,9 @@ function runge_kutta_fehlberg(f,a::Float64,b::Float64,tol::Float64, Kmax::Float6
         end
     end
     resize!(t, i)
-    resize!(U, i)
-    return t, U
+    resize!(X, i)
+    resize!(Y, i)
+    return t, X, Y
 end
 
 
